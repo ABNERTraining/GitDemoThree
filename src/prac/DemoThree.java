82fe7e1af058aef64a47b8a8aafcb667b122c5ea
ABNER <ABNER@ABNER.lan>
2021-09-30 21:14:58 -0400

    addition

--- src/prac/DemoThree.java
+++ src/prac/DemoThree.java
@@ -3,5 +3,12 @@ package prac;
 public class DemoThree {
 	public static void main(String[] args) {
 		System.out.println("Prac prac...");
+		
+		boolean isSunday = true;
+		if(isSunday) {
+			System.out.println("ABNER");
+		}else {
+			System.out.println("RENBA");
+		}
 	}
 }
